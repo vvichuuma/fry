5b4fe9303b90946f7b45704dea4dfe9333718e64
“Vishnu <vvichugit@gmail.com>
2018-09-09 22:19:42 -0500

    my first initial commit

--- src/router.js
+++ src/router.js
@@ -1,23 +1,31 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import result from "./views/results.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/result",
+      name: "result",
+      component: result
+    },
+
+    {
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue")
     }
   ]
-})
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,119 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 id="db">{{ message }}</h1>
+    
+
+
+    <h5>Who is the most intelligent?</h5>
+
+    <div v-for="choice in choices" class="radio-inline">
+    <input type="radio" v-bind:value="choice" v-on:click="add(choice)" name="optradio1">
+    {{ choice }}
+    </div>
+
+
+      <h5>Who is the most powerful?</h5>
+
+    <div v-for="choice in choices" class="radio-inline">
+    <input type="radio" v-bind:value="choice" v-on:click="add(choice)" name="optradio2">
+    {{ choice }}
+    </div>
+
+
+    <h5>Who is the most strongest?</h5>
+
+
+    <div v-for="choice in choices" class="radio-inline">
+    <input type="radio" v-bind:value="choice" v-on:click="add(choice)" name="optradio3">
+    {{ choice }}
+    </div>
+
+
+    <h5>Who is the most Fastest man alive on Earth?</h5>
+
+
+    <div v-for="choice in choices" class="radio-inline">
+    <input type="radio" v-bind:value="choice" v-on:click="add(choice)" name="optradio4">
+    {{ choice }}
+    </div>
+
+
+    <h5>Who is the most Fastest man alive in outerspace?</h5>
+
+
+    <div v-for="choice in choices" class="radio-inline">
+    <input type="radio" v-bind:value="choice" v-on:click="add(choice)" name="optradio5">
+    {{ choice }}
+    </div>
+
+
+   
+    <h5>Who can defy gravity?</h5>
+
+
+    <div v-for="choice in choices" class="radio-inline">
+    <input type="radio" v-bind:value="choice" v-on:click="add(choice)" name="optradio6">
+    {{ choice }}
+    </div>
+
+
+
+     <button type="button" v-on:click="calc()" >Calculate</button>
+
+     <div id="sup"></div>
+
+
+
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+ 
+ #db{
 
+    text-align: center;
+
+ }
+
+</style>
+
+<script>
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Dg Questions",
+      ans:"",
+      choices: ["Bruce","Clark","Barry"],
+      answer:[],
+      zombie:[]
+
+    };
+  },
+  created: function() {},
+  methods: {
+
+    add :function(input) {
+
+      console.log(input);
+      this.ans = input;
+      console.log(this.ans);
+      this.answer.push(input);
+      console.log(this.answer);
+
+    },
+
+
+    calc :function() {
+      
+       document.getElementById("sup").innerHTML = ("Vishnu is a programmer");
+
+
+
+
+    }
+
+
+  },
+  computed: {}
+};
 </script>
